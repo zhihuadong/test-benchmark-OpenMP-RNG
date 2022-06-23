0f5972eb6d36d38e72d19b45fc0b306d8e695b1c
Tianle Wang <twang@lambda1.csi.bnl.gov>
2022-06-23 13:33:29 -0400

    Add support for cout generator_enum

--- implementation/useful_enum.h
+++ implementation/useful_enum.h
@@ -1,16 +1,7 @@
 #ifndef _USEFUL_ENUM_
 #define _USEFUL_ENUM_
 
-enum class generator_enum
-{
-  philox,
-  xorwow,
-  mrg32k3a,
-  sobol32,
-  sobol64,
-  mtgp32,
-  mt19937
-};
+#include<cassert>
 
 /* For each backend, below is the support table of each generator
  *
@@ -24,5 +15,47 @@ enum class generator_enum
  * mt19937    False       True    False
  *
  */
+
+enum class generator_enum
+{
+  philox,
+  xorwow,
+  mrg32k3a,
+  sobol32,
+  sobol64,
+  mtgp32,
+  mt19937
+};
+
+inline std::ostream& operator<<(std::ostream & os, const generator_enum & gen)
+{
+  switch(gen)
+  {
+    case generator_enum::philox:
+      os << "philox";
+      break;
+    case generator_enum::xorwow:
+      os << "xorwow";
+      break;
+    case generator_enum::mrg32k3a:
+      os << "mrg32k3a";
+      break;
+    case generator_enum::sobol32:
+      os << "sobol32";
+      break;
+    case generator_enum::sobol64:
+      os << "sobol64";
+      break;
+    case generator_enum::mtgp32:
+      os << "mtgp32";
+      break;
+    case generator_enum::mt19937:
+      os << "mt19937";
+      break;
+    default:
+      os << "Error: Unrecognized generator_enum";
+      assert(0);
+  }
+}
 
 #endif
